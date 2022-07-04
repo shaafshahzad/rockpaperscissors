b643954480d07e2d9c16c86df4636bb6c58a876d
Shaaf <shaaf.m.shahzad@gmail.com>
2022-07-04 01:42:24 -0400

    Added function to determine winner

--- rps.js
+++ rps.js
@@ -2,19 +2,19 @@ const choice = ['rock', 'paper', 'scissors'];
 let playerPoints = 0
 let computerPoints = 0
 const buttons = document.querySelectorAll('input')
+let outcome = '';
+let totalScore = '';
+let i = 0;
 
-function game(){
-    playRound();
-}
-
-function computerPlay(){
-    return choice[Math.floor(Math.random() * choice.length)];
+function startGame(){
+    while (i <= 5) {
+        playRound();
+        i += 1;
+    }
 }
 
 function playRound(playerSelection){
     const computerSelection = computerPlay();
-    let outcome = '';
-    let totalScore = '';
 
     if (playerSelection == 'rock' && computerSelection == 'scissors' ||
         playerSelection == 'paper' && computerSelection == 'rock' ||
@@ -23,10 +23,6 @@ function playRound(playerSelection){
         playerPoints += 1;
         outcome = ('Player wins. ' + playerSelection + ' beats ' + computerSelection);
         totalScore = ("<br><br>Player: " + playerPoints + "<br>Computer: " + computerPoints);
-
-        if (playerPoints == 5){
-            outcome = ('Player wins the game.')
-        }
  
     } else if (playerSelection == computerSelection){
         outcome = ('Tie. Both player and computer picked the same object.')
@@ -37,15 +33,35 @@ function playRound(playerSelection){
         outcome = ('Computer wins. ' + computerSelection + ' beats ' + playerSelection);
         totalScore = ("<br><br>Player: " + playerPoints + "<br>Computer: " + computerPoints);
 
-        if (computerPoints == 5){
-            outcome = ('Computer wins the game.');
-        }        
     }
 
     document.getElementById('outcome').innerHTML = outcome;
     document.getElementById('totalScore').innerHTML = totalScore;
+    checkWinner();
     return;
 
+}
+
+function checkWinner(){
+    if (playerPoints === 3) {
+        outcome = 'Player wins.';
+        console.log('hello');
+        disableButtons();
+    } else if (computerPoints === 3) {
+        outcome = 'Computer wins.';
+        console.log('hello');
+        disableButtons();
+    }
+}
+
+function computerPlay(){
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
 buttons.forEach(button =>{
@@ -53,5 +69,3 @@ buttons.forEach(button =>{
         playRound(button.value);
     })
 })
-
-
